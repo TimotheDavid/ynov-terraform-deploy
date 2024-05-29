2cd0715229a6c1c3521ee5128baf5a34eef0832b
Timothe David <timoth.david@gmail.com>
2024-05-29 10:57:36 +0200

    add .gitignore

---
+++ main.tf
@@ -0,0 +1,37 @@
+terraform {
+  # This module is now only being tested with Terraform 0.13.x. However, to make upgrading easier, we are setting
+  # 0.12.26 as the minimum version, as that version added support for required_providers with source URLs, making it
+  # forwards compatible with 0.13.x code.
+  required_version = ">= 0.12.26"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "ynov-tr-group"
+  location = "West Europe"
+}
+
+resource "azurerm_service_plan" "plan" {
+  name                = "web-app-ynov-tr"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+    sku_name = "B1"
+  os_type = "Linux"
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "ynovtrstorage"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
